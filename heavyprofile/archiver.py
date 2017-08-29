366d3074e20ed55e105b4b500ce7addfcb365402
Tarek Ziadé <tarek@ziade.org>
2017-08-29 15:30:12 +0200

    producing a first tarball

--- heavyprofile/archiver.py
+++ heavyprofile/archiver.py
@@ -2,10 +2,28 @@
 """
 import sys
 import argparse
+import tarfile
+import os
+import glob
+from datetime import date
+
+
+def _log(msg):
+    print(msg)
 
 
 def create_archives(profile_dir, archives_dir):
-    pass
+    today = date.today()
+    archive = today.strftime('%Y-%m-%d-hp.tar.gz')
+    _log("Creating %s..." % archive)
+    archive = os.path.join(archives_dir, archive)
+
+    with tarfile.open(archive, "w:gz") as tar:
+        for filename in glob.glob(os.path.join(profile_dir, "*")):
+            _log("\tAdding %s..." % filename)
+            tar.add(filename, os.path.basename(filename))
+
+    _log("Done.")
 
 
 def main(args=sys.argv[1:]):
--- heavyprofile/tests/test_archiver.py
+++ heavyprofile/tests/test_archiver.py
@@ -1,14 +1,21 @@
+import os
 import unittest
 import shutil
+
 from heavyprofile.tests.support import fresh_profile
+from heavyprofile.archiver import create_archives
+import tempfile
 
 
 class TestArchiver(unittest.TestCase):
     def setUp(self):
         self.profile, self.profile_dir = fresh_profile()
+        self.archives_dir = os.path.join(tempfile.mkdtemp())
 
     def tearDown(self):
         shutil.rmtree(self.profile_dir)
+        shutil.rmtree(self.archives_dir)
 
     def test_simple_archiving(self):
-        pass
+        create_archives(self.profile_dir, self.archives_dir)
+        self.assertTrue(len(os.listdir(self.archives_dir)), 3)
