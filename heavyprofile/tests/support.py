edd424fd0e1356dd56134492bf66687310d942f5
Tarek Ziadé <tarek@ziade.org>
2017-08-29 15:18:57 +0200

    hooked the cloning of the base profile

--- heavyprofile/tests/support.py
+++ heavyprofile/tests/support.py
@@ -1,10 +1,13 @@
 import os
+import tempfile
 from mozprofile.profile import Profile
 
 
 _BASE_PROFILE = os.path.join(os.path.dirname(__file__), 'base_profile')
 
 
-def clone_profile(target_dir):
+def fresh_profile(target_dir=None):
+    if target_dir is None:
+        target_dir = os.path.join(tempfile.mkdtemp(), 'profile')
     p = Profile.clone(_BASE_PROFILE, target_dir, restore=False)
-    return p
+    return p, target_dir
--- heavyprofile/tests/test_archiver.py
+++ heavyprofile/tests/test_archiver.py
@@ -1,6 +1,14 @@
 import unittest
+import shutil
+from heavyprofile.tests.support import fresh_profile
 
 
 class TestArchiver(unittest.TestCase):
+    def setUp(self):
+        self.profile, self.profile_dir = fresh_profile()
+
+    def tearDown(self):
+        shutil.rmtree(self.profile_dir)
+
     def test_simple_archiving(self):
         pass
