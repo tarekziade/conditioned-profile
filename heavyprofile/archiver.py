b20ea323770075752a7fce66e0908bdd1d7d4a27
Tarek Ziadé <tarek@ziade.org>
2017-08-29 17:36:50 +0200

    started a diff info file

--- heavyprofile/archiver.py
+++ heavyprofile/archiver.py
@@ -5,16 +5,56 @@ import argparse
 import tarfile
 import os
 import glob
-from datetime import date
+from datetime import date, timedelta
+import tempfile
 
 
 def _log(msg):
     print(msg)
 
 
-def update_archives(profile_dir, archives_dir):
-    today = date.today()
-    archive = today.strftime('%Y-%m-%d-hp.tar.gz')
+def create_diff(archives_dir, when, current, previous):
+    current_files = {}
+    previous_files = {}
+    diff_info = []
+
+    with tarfile.open(current, "r:gz") as tar:
+        for tarinfo in tar:
+            current_files[tarinfo.name] = tarinfo
+        with tarfile.open(previous, "r:gz") as tar:
+            for tarinfo in tar:
+                previous_files[tarinfo.name] = tarinfo
+
+        for name, info in current_files.items():
+            if name not in previous_files:
+                diff_info.append("NEW:%s" % name)
+            else:
+                # XXX need to compare them
+                pass
+        for name, info in previous_files.items():
+            if name not in current_files:
+                diff_info.append("DELETED:%s" % name)
+
+    day_before = when - timedelta(days=1)
+    diff_archive = 'diff-%s-%s-hp.tar.gz' % (day_before.strftime('%Y-%m-%d'),
+                                             when.strftime('%Y-%m-%d'))
+    diff_archive = os.path.join(archives_dir, diff_archive)
+    handle, diff_info_file = tempfile.mkstemp()
+    os.close(handle)
+
+    with open(diff_info_file, 'w') as f:
+        for line in diff_info:
+            f.write(line + '\n')
+
+    with tarfile.open(diff_archive, "w:gz") as tar:
+        tar.add(diff_info_file, arcname="diffinfo")
+
+
+def update_archives(profile_dir, archives_dir, when=None):
+    if when is None:
+        when = date.today()
+    day_before = when - timedelta(days=1)
+    archive = when.strftime('%Y-%m-%d-hp.tar.gz')
     _log("Creating %s..." % archive)
     archive = os.path.join(archives_dir, archive)
 
@@ -24,9 +64,15 @@ def update_archives(profile_dir, archives_dir):
             tar.add(filename, os.path.basename(filename))
 
     _log("Done.")
-    os.symlink(archive, os.path.join(archives_dir, 'latest.tar.gz'))
-
-    # XXX create a diff if we have day -1 present here
+    latest = os.path.join(archives_dir, 'latest.tar.gz')
+    if os.path.exists(latest):
+        os.remove(latest)
+    os.symlink(archive, latest)
+    previous = os.path.join(archives_dir,
+                            day_before.strftime('%Y-%m-%d-hp.tar.gz'))
+    if os.path.exists(previous):
+        _log("Creating a diff tarball with the previous day")
+        create_diff(archives_dir, when, archive, previous)
 
 
 def main(args=sys.argv[1:]):
--- heavyprofile/tests/test_archiver.py
+++ heavyprofile/tests/test_archiver.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 import shutil
-from datetime import date
+from datetime import date, timedelta
 
 from heavyprofile.tests.support import fresh_profile
 from heavyprofile.archiver import update_archives
@@ -25,6 +25,28 @@ class TestArchiver(unittest.TestCase):
         res.sort()
         today = date.today()
         archive = today.strftime('%Y-%m-%d-hp.tar.gz')
-
         wanted = [archive, 'latest.tar.gz']
+        self.assertEqual(res, wanted)
+
+    def test_diff_archiving(self):
+        # we update the archives every day for 15 days
+        # we keep the last ten days
+        wanted = ['latest.tar.gz']
+        _15_days_ago = date.today() - timedelta(days=15)
+
+        for i in range(15):
+            when = _15_days_ago + timedelta(days=i)
+            wanted.append(when.strftime('%Y-%m-%d-hp.tar.gz'))
+            if i != 0:
+                then = when - timedelta(days=1)
+                then_str = then.strftime('%Y-%m-%d')
+                when_str = when.strftime('%Y-%m-%d')
+                name = 'diff-%s-%s-hp.tar.gz' % (then_str, when_str)
+                wanted.append(name)
+
+            update_archives(self.profile_dir, self.archives_dir, when)
+
+        wanted.sort()
+        res = os.listdir(self.archives_dir)
+        res.sort()
         self.assertEqual(res, wanted)
