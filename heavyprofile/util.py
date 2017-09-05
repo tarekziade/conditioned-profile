856fdf90c269d1d7104e70afd0803f1d15967aa9
Tarek Ziadé <tarek@ziade.org>
2017-09-05 09:52:00 +0200

    added initial support for linux

--- heavyprofile/util.py
+++ heavyprofile/util.py
@@ -1,3 +1,4 @@
+import platform
 import time
 import os
 import tempfile
@@ -79,11 +80,19 @@ link = 'https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/'
 
 
 def get_firefox_download_link():
+    if platform.system() == 'Darwin':
+        extension = '.dmg'
+    elif platform.system() == 'Linux':
+        arch = platform.machine()
+        extension = '.linux-%s.tar.bz2' % arch
+    else:
+        raise NotImplementedError(platform.system())
+
     page = requests.get(link).text
     soup = BeautifulSoup(page, "html.parser")
     for node in soup.find_all('a', href=True):
         href = node['href']
-        if href.endswith('.dmg'):
+        if href.endswith(extension):
             return 'https://ftp.mozilla.org' + href
     raise Exception()
 
@@ -142,15 +151,24 @@ def download_file(url, target=None, check_file=True):
 def latest_nightly(binary=None):
     if binary is None:
         # we want to use the latest nightly
-        # mac os specific for now
         nightly_archive = get_firefox_download_link()
         logger.msg("Downloading %s" % nightly_archive)
         target = download_file(nightly_archive, check_file=False)
-        cmd = "hdiutil attach -mountpoint /Volumes/Firefox %s"
-        os.system(cmd % target)
-        # now that the dmg is mounted, we can use it
-        binary = ('/Volumes/Firefox/FirefoxNightly.app'
-                  '/Contents/MacOS/firefox')
+
+        # on macOs we just mount the DMG
+        if platform.system() == 'Darwin':
+            cmd = "hdiutil attach -mountpoint /Volumes/Firefox %s"
+            os.system(cmd % target)
+            binary = ('/Volumes/Firefox/FirefoxNightly.app'
+                      '/Contents/MacOS/firefox')
+        # on linux we unpack it
+        elif platform.system() == 'Linux':
+            cmd = 'bunzip2 %s' % target
+            os.system(cmd)
+            cmd = 'tar -xvf %s' % target[:-len('.bz2')]
+            os.system(cmd)
+            binary = 'firefox/firefox'
+
         mounted = True
     else:
         mounted = False
@@ -158,6 +176,10 @@ def latest_nightly(binary=None):
         yield binary
     finally:
         if mounted:
-            logger.msg("Unmounting Firefox")
-            time.sleep(10)
-            os.system("hdiutil detach /Volumes/Firefox")
+            if platform.system() == 'Darwin':
+                logger.msg("Unmounting Firefox")
+                time.sleep(10)
+                os.system("hdiutil detach /Volumes/Firefox")
+            elif platform.system() == 'Linux':
+                # XXX we should keep it for next time
+                os.rmtree('firefox')
